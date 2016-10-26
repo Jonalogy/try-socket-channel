2b6f27e06ec92d3ae5c602826515781e59e84cd1
Jonalogy <jonalogy@gmail.com>
2016-10-26 13:57:27 +0800

    Renamed index.js to server.js

---
+++ server.js
@@ -0,0 +1,46 @@
+var morgan = require('morgan');
+var express = require('express');
+var path = express('path')
+
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+// const http = require('http');
+// const server = http.createServer(app)
+// const io = require('socket.io')(server)
+app.use(morgan('combined'))
+
+// Setting static directory for the views
+  app.use(express.static('public'));
+
+// Controllers
+  app.get('/', function(req, res){
+    res.sendFile('index.html');
+  });
+
+//Socket Namespaces
+
+//
+var userOn = []; //Socket.id containters
+
+io.on('connection',function(socket){
+  var user = {}
+  user[`user${userOn.length}`] = socket.id;
+  userOn.push( user );
+
+  socket.broadcast.emit('incomer',{welcome:`New User of id:${socket.id} Joined!`});
+  io.emit('list-all',{users: userOn}); // format-> {users: [{}]}
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect',()=>{
+    console.log('user disconnected');
+  })
+});
+
+http.listen(3000,()=>{
+  console.log('try-socket.app: Ready on localhost:3000');
+})
