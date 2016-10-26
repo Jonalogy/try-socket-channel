df3b07864cf83431ed134997a6b1414f667c4ddb
Jonalogy <jonalogy@gmail.com>
2016-10-26 18:19:29 +0800

    Successfully implemented Socket rooms

--- public/client.js
+++ public/client.js
@@ -1,37 +1,84 @@
+var roomInit = false;
+var socket = io();
+
 $(document).ready(()=>{
-  var socket = io();
 
-  socket.on('incomer',(data)=>{ // New user prompt
-    $('#messages').append($('<li>').text(`>>Tada , ${data.welcome}`));
-  })
-
-  socket.on('list-all',(users)=>{
-    var allUsers = ''
-    console.log(`>>users, ${users.users.length}`);
-    users.users.forEach((user)=>{
-
-      for(var key in user){
-        console.log(key)
-        allUsers += `${key}, `
-
-      }
-    });
-
-    $('#messages').append($('<li>').text(`>>Users online: ${allUsers}`));
-  });
-
-  socket.on('chat message', function(msg){
-    $('#messages').append($('<li>').text(msg));
-  });
+  chatGlobal();
 
 //----Events----
+  $('#room1').click(()=>{
+      if (roomInit == false){
+      console.log(socket.id + " is joining a room");
+      socket.emit('create', 'room');
+      chatRoom();
+      roomInit = true;
+    }
+  })
+
+  $('#leaveRoom').click(()=>{
+    if(roomInit == true) {
+      socket.emit('leaveRoom', socket.id);
+    }
+  })
+
   $('#get_user').click(()=>{
     console.log(`hello, ${$('#username').val()}`)
   })
 
-  $('form').submit(function(){
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
+  $('#formGlobal').submit(function(){
+    socket.emit('global message', $('#inputGlobal').val());
+    $('#inputGlobal').val('');
     return false;
   });
+
 }) //End of DOM content loaded
+
+function chatGlobal(){
+    console.log(socket)
+    socket.on('incomer',(data)=>{ // New user prompt
+      $('#global').append($('<li>').text(`>>Tada , ${data.welcome}`));
+    })
+
+    socket.on('global message', function(msg){
+      $('#global').append($('<li>').text(msg));
+    });
+
+    socket.on('list-all',(users)=>{
+      var allUsers = ''
+      console.log(`>>users, ${users.users.length}`);
+      users.users.forEach((user)=>{
+        console.log(user)
+
+        for(var key in user){
+          allUsers += `${key}, `
+        }//end of forEach
+      });
+      $('#messages').append($('<li>').text(`>>Users online: ${allUsers}`));
+    });
+}
+
+function chatRoom(){
+  var roomNo;
+
+  socket.on('joined-room', (room)=>{
+    roomNo = room;
+    console.log(`${socket.id} joined ${roomNo.room},`)
+    $('#room').append($('<li>').text(room.room));
+  });
+
+  socket.on('room-message', (msg)=>{
+    $('#room').append($('<li>').text(msg));
+  });
+
+  $('#formRoom').submit(function(){
+      var msg = $('#inputRoom').val()
+      socket.emit('room1 message',
+        { room: roomNo,
+          msg: msg }
+    );
+    $('#inputRoom').val('');
+    return false;
+  });
+
+
+}
--- server.js
+++ server.js
@@ -18,27 +18,49 @@ app.use(morgan('combined'))
     res.sendFile('index.html');
   });
 
-//Socket Namespaces
-
-//
-var userOn = []; //Socket.id containters
+//Socket
+  var roomNo = 1;
+  var userOn = []; //Socket.id containters
 
 io.on('connection',function(socket){
-  var user = {}
-  user[`user${userOn.length}`] = socket.id;
-  userOn.push( user );
 
-  socket.broadcast.emit('incomer',{welcome:`New User of id:${socket.id} Joined!`});
-  io.emit('list-all',{users: userOn}); // format-> {users: [{}]}
+  //----Global Settings
+    var user = {}
+    user[`user${userOn.length}`] = socket.id;
+    userOn.push( user );
 
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
+    socket.broadcast.emit('incomer',{welcome:`New User_id: ${socket.id} Joined!`});
+    io.emit('list-all',{users: userOn}); // format-> {users: [{}]}
 
-  socket.on('disconnect',()=>{
-    console.log('user disconnected');
-  })
+
+    socket.on('global message', function(msg){
+      console.log('>>Message:', msg)
+      io.emit('global message', msg);
+    });
+
+  //----Room Settings----
+    socket.on('create', function(room) {
+      var entry = room + roomNo
+      socket.join(entry);
+      console.log('>>>Checking Rooms:', socket.adapter.rooms);
+      socket.emit('joined-room',{room: entry})
+    });
+
+    socket.on('room1 message', function(data){
+      console.log(`>>Message to ${data.room}: ${data.msg}`)
+      io.to('room1').emit('room-message', data.msg);
+    });
+
+    socket.on('leaveRoom',()=>{
+      socket.leave('room1')
+      console.log('>>>Leave Rooms:', socket.adapter.rooms);
+    })
+
+    socket.on('disconnect',()=>{
+      console.log('user disconnected');
+      console.log('>>Remaining Clients: ', socket.adapter.rooms)
+    })
+
 });
 
 http.listen(3000,()=>{
